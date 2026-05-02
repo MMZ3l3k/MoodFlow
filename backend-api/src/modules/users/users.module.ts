a5e4eea9b5b470dcbd4ef8f6e0958d0bf588a537
Mateusz Matczuk <113317915+MMZ3l3k@users.noreply.github.com>
2026-05-02 23:20:45 +0200

    fix: import NotificationsModule w UsersModule

--- backend-api/src/modules/users/users.module.ts
+++ backend-api/src/modules/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { AuditModule } from '../audit/audit.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AuditModule],
+  imports: [TypeOrmModule.forFeature([User]), AuditModule, NotificationsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
